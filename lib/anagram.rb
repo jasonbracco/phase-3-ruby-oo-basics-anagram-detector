ddbf4143a46fc04b18d7206c3b1e0c61096c3972
Jason Bracco <bracco.jason@gmail.com>
2022-08-23 22:29:04 -0400

    commit tree 2ad8ddd63598c34a3d21984f7102658e7715595a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(array)
+        array.filter do |string|
+            if @word.chars.sort == string.chars.sort
+                string
+            end
+        end
+
+    end
+end
